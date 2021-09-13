85c52899de819d642b3a403fe1d0e68ed441a47e
joneganagranado <joneganagranado@hotmail.com>
2021-09-13 19:22:52 +0000

    empezar con los ejercicios

--- exercises/05.4-Add-items-to-array-looping/app.js
+++ exercises/05.4-Add-items-to-array-looping/app.js
@@ -5,3 +5,13 @@ var arr = [4,5,734,43,45];
 // you need to loop 10 times, for example, using a for loop
 //*****************
 
+function addRandomIntegers(arr,num){
+    if(num>0){
+        for(var i=num;i>0;i--){
+            arr.push(Math.floor((Math.random() * 10) + 1))
+        }
+    }
+    return arr;
+}
+
+console.log(addRandomIntegers(arr,10))
